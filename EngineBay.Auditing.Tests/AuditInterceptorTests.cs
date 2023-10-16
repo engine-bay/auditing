a0777a062121ef6ae329430a3911101bece524c1
GreatMar2 <greatmar2@gmail.com>
2023-10-16 16:53:49 +0100

    Auditing create and update tests working

--- EngineBay.Auditing.Tests/AuditInterceptorTests.cs
+++ EngineBay.Auditing.Tests/AuditInterceptorTests.cs
@@ -8,6 +8,9 @@
 
     public class AuditInterceptorTests : BaseTestWithDbContext<AuditingWriteDbContext>
     {
+        private readonly List<FakeModel> fakeModels;
+        private readonly List<ApplicationUser> applicationUsers;
+
         public AuditInterceptorTests()
             : base(nameof(AuditInterceptorTests))
         {
@@ -27,46 +30,78 @@
             this.FakeDbContext.Database.EnsureCreated();
 
             var applicationUsersPath = Path.GetFullPath(@"./TestData/application-users.json");
-            var applicationUsers = JsonConvert.DeserializeObject<List<ApplicationUser>>(File.ReadAllText(applicationUsersPath));
-            if (applicationUsers == null || this.FakeDbContext.ApplicationUsers.Any())
-            {
-                return;
-            }
+
+            var tempApplicationUsers = JsonConvert.DeserializeObject<List<ApplicationUser>>(File.ReadAllText(applicationUsersPath));
+            ArgumentNullException.ThrowIfNull(tempApplicationUsers);
+            this.applicationUsers = tempApplicationUsers;
 
             var fakeModelPath = Path.GetFullPath(@"./TestData/fake-models.json");
-            var fakeModels = JsonConvert.DeserializeObject<List<FakeModel>>(File.ReadAllText(fakeModelPath));
-            if (fakeModels == null || this.FakeDbContext.FakeModels.Any())
-            {
-                return;
-            }
-
-            this.FakeDbContext.AddRange(applicationUsers);
-            this.FakeDbContext.SaveChanges(this.ApplicationUser);
-            this.FakeDbContext.AddRange(fakeModels);
-            this.FakeDbContext.SaveChanges(this.ApplicationUser);
+            var tempFakeModels = JsonConvert.DeserializeObject<List<FakeModel>>(File.ReadAllText(fakeModelPath));
+            ArgumentNullException.ThrowIfNull(tempFakeModels);
+            this.fakeModels = tempFakeModels;
         }
 
         protected FakeDbContext FakeDbContext { get; set; }
 
-        [Fact]
-        public void WillAuditChangesOnUpdate()
+        [Theory]
+        [InlineData("6b29309a-f273-43f0-ad31-b88060e6d684", "one", "two")]
+        [InlineData("b54636a8-26ba-4b09-bd4e-50bcffa50a1b", "three", "four")]
+        [InlineData("b9d2a805-64cf-4719-ab83-568fca9dc820", "five", "six")]
+        public void WillAuditChangesOnSave(string id, string name, string description)
         {
-            var identity = new MockCurrentIdentity();
-            var numberOfAudits = this.DbContext.AuditEntries.Count();
+            this.ResetDbs();
 
-            var idToTest = Guid.Parse("b9274cd3-c673-4656-b5d3-d8b20df41ad4");
+            var modelToSave = new FakeModel
+            {
+                Id = Guid.Parse(id),
+                Name = name,
+                Description = description,
+            };
+
+            this.FakeDbContext.Add(modelToSave);
+            this.FakeDbContext.SaveChanges();
+
+            var audit = this.DbContext.AuditEntries.Single(x => x.EntityId == id.ToString());
+            Assert.NotNull(audit);
+            Assert.Contains(name, audit.Changes);
+            Assert.Contains(description, audit.Changes);
+        }
+
+        [Theory]
+        [InlineData("b9274cd3-c673-4656-b5d3-d8b20df41ad4")]
+        [InlineData("7710a98b-a15e-4ba2-9a5e-8aba30a29033")]
+        [InlineData("0407c397-af0a-475b-92a2-87570c6e6d02")]
+        public void WillAuditChangesOnUpdate(string id)
+        {
+            this.ResetDbs();
+
+            var idToTest = Guid.Parse(id);
             var modelToupdate = this.FakeDbContext.FakeModels.Find(idToTest);
-            var oldDescription = modelToupdate.Description;
+
+            Assert.NotNull(modelToupdate);
 
             var newDescription = modelToupdate.Description + " - updated";
             modelToupdate.Description = newDescription;
 
             this.FakeDbContext.SaveChanges();
 
-            var newNumberOfAudits = this.DbContext.AuditEntries.Count();
-            var newAudit = this.DbContext.AuditEntries.Single(x => x.EntityId == idToTest.ToString() && x.ActionType == "UPDATE");
+            var numberOfAudits = this.DbContext.AuditEntries.Count();
+            var audit = this.DbContext.AuditEntries.Single(x => x.EntityId == idToTest.ToString());
 
-            Assert.Equal(numberOfAudits + 1, newNumberOfAudits);
+            Assert.Equal(1, numberOfAudits);
+            Assert.NotNull(audit);
+            Assert.Contains(newDescription, audit.Changes);
+        }
+
+        private void ResetDbs()
+        {
+            this.FakeDbContext.AddRange(this.applicationUsers);
+            this.FakeDbContext.SaveChanges(this.ApplicationUser);
+            this.FakeDbContext.AddRange(this.fakeModels);
+            this.FakeDbContext.SaveChanges(this.ApplicationUser);
+
+            this.DbContext.AuditEntries.RemoveRange(this.DbContext.AuditEntries);
+            this.DbContext.SaveChanges();
         }
 
         // [Fact]
