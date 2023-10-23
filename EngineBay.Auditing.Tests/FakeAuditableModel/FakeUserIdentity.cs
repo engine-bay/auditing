572af64a3fe586506f0055d799397c0903fa1e3d
GreatMar2 <greatmar2@gmail.com>
2023-10-23 13:37:00 +0100

    Add github actions yamls

---
+++ EngineBay.Auditing.Tests/FakeAuditableModel/FakeUserIdentity.cs
@@ -0,0 +1,17 @@
+﻿namespace EngineBay.Auditing
+{
+    using EngineBay.Core;
+
+    public class FakeUserIdentity : ICurrentIdentity
+    {
+        public FakeUserIdentity()
+        {
+            this.UserId = Guid.Empty;
+            this.Username = "FakeUser";
+        }
+
+        public Guid UserId { get; }
+
+        public string? Username { get; }
+    }
+}
